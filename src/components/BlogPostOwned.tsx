1dff7f96355102efb48425929fe83132a2d70c29
Genesis Lovino <i_genesis.lovino@stratpoint.com>
2025-07-15 13:13:59 +0800

    Added text if user hasn't made any blogs

--- src/components/BlogPostOwned.tsx
+++ src/components/BlogPostOwned.tsx
@@ -9,19 +9,27 @@ interface BlogPostOwnedProps {
 const BlogPostOwned: React.FC<BlogPostOwnedProps> = ({ blogs }) => {
     return (
         <div className="flex flex-col gap-4 pt-4 pb-32">
-            {blogs.map((blog) => (
-                <div key={blog.id} className="p-4 border rounded-lg">
-                    <Link href={`/${blog.slug}`}>
-                        <h2 className="text-xl font-bold hover:underline">{blog.title}</h2>
-                    </Link>
-                    <p className="text-sm text-gray-500">
-                        Last updated:{" "}
-                        {blog.updated_at
-                            ? new Date(blog.updated_at).toLocaleDateString()
-                            : "N/A"}
-                    </p>
+            {blogs.length == 0 ? (
+                <div>
+                    <p>{"You don't have any blogs posted."}</p>
                 </div>
-            ))}
+            ) : (
+                blogs.map((blog) => (
+                    <div key={blog.id} className="p-4 border rounded-lg">
+                        <Link href={`/${blog.slug}`}>
+                            <h2 className="text-xl font-bold hover:underline">
+                                {blog.title}
+                            </h2>
+                        </Link>
+                        <p className="text-sm text-gray-500">
+                            Last updated:{" "}
+                            {blog.updated_at
+                                ? new Date(blog.updated_at).toLocaleDateString()
+                                : "N/A"}
+                        </p>
+                    </div>
+                ))
+            )}
         </div>
     );
 };
